42b84af46231fd4141472b0b569f76cf146f82b4
Xtrike <felipe.g.vargas.12@gmail.com>
2026-05-20 01:21:43 -0400

    wq

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const juegos = [
+    { id: 1, nombre: "GTA V", categoria: "Acción" },
+    { id: 2, nombre: "FIFA 24", categoria: "Deportes" },
+    { id: 3, nombre: "Minecraft", categoria: "Sandbox" }
+];
+
+const app = {
+    favoritos: []
+};
